2067f6180644f7503cfb6e2b967b50bcdb3016e2
Masyanya <ypposm@mailto.plus>
2022-11-10 00:04:20 +0300

    Second commit with class

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -8,30 +8,27 @@ from aiogram.types import ReplyKeyboardRemove, \
 
 
 
-def sql_start():
-	global base, cur
-	base =sq.connect('Muxomor.db')
-	cur = base.cursor()
-	if base:
-		print('Data base connected OK')
+class BotDB:
+
+    def __init__(self,db_file):
+        self.conn = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
+
+	def create_start():
 	base.execute('CREATE TABLE IF NOT EXISTS menu(id INTEGER PRIMARY KEY, category TEXT, name TEXT, description TEXT, price TEXT)')
 	#base.execute('CREATE TABLE IF NOT EXISTS korz(id INTEGER PRIMARY KEY, category TEXT, description TEXT, price TEXT)')
 	base.commit()
 
-async def sql_add_command(state):
-	async with state.proxy() as data:
-		cur.execute('INSERT INTO menu VALUES (?, ?, ?, ?)', tuple(data.values()))
-		base.commit()
+	async def sql_add_command(state):
+		async with state.proxy() as data:
+			cur.execute('INSERT INTO menu VALUES (?, ?, ?, ?)', tuple(data.values()))
+			base.commit()
 
-async def sql_imp_command(message):
-	for ret in cur.execute("SELECT * FROM menu").fetchall():
-		await bot.send_message(message.from_user.id, f"Название {ret[0]}\nКатегория {ret[1]}\nОписание {ret[2]}\nЦена {ret[3]}")
+	async def sql_imp_command(message):
+		for ret in cur.execute("SELECT * FROM menu").fetchall():
+			await bot.send_message(message.from_user.id, f"Название {ret[0]}\nКатегория {ret[1]}\nОписание {ret[2]}\nЦена {ret[3]}")
 
 
-async def sql_show_name(message):
-	for ret in cur.execute("SELECT name FROM menu").fetchall():
-		await bot.send_message(message.from_user.id,f'{ret[0]}', reply_markup= client_kb.main_menu)
-
-	# first_one = res[0]
-	# return first_one
-
+	async def sql_show_name(message):
+		for ret in cur.execute("SELECT name FROM menu").fetchall():
+			return ret[0]
