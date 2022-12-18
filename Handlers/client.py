40e38d395d131ec2658f870ad7d2d5cbd4553e9c
user <ypposm@malito.plus>
2022-12-18 16:07:16 +0300

    that's commit

--- Handlers/client.py
+++ Handlers/client.py
@@ -1,4 +1,7 @@
+from contextlib import suppress
 from aiogram import types,Dispatcher
+from aiogram.utils.exceptions import MessageNotModified
+
 from Handlers.create_bot import dp, bot
 from Keyboards import client_kb, show_case_kb
 from aiogram.types import ReplyKeyboardRemove
@@ -15,14 +18,12 @@ async def command_start(message: types.Message):
 
 async def show_case(message:types.Message):
     if message.text == 'Каталог':
-        sol = await sqlite_db.sql_show_name(message)
-        await bot.send_message(message.from_user.id,f'{sol}', reply_markup= client_kb.main_menu)
         await bot.send_message(message.from_user.id, 'Выберите категорию продукта', reply_markup = show_case_kb.inline_kb)
 
     if message.text == 'Главное меню':
         await bot.send_message(message.from_user.id,'Ты вернулся в главное меню.', reply_markup= client_kb.kb_client)
     if message.text =='Добавить в корзину':
-        await bot.send_message(message.from_user.id, ' ',reply_markup = show_case_kb.inline_kb4)
+        await bot.send_message(message.from_user.id, ' ', reply_markup = show_case_kb.keyb(2))
 
 
 @dp.callback_query_handler(lambda c: c.data == 'button1')
@@ -41,28 +42,42 @@ async def process_callback_button1(callback_query: types.CallbackQuery):
 async def process_callback_button1(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, 'Мухомор красный ',reply_markup = show_case_kb.inline_kb3)
-    await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
+    #await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
 
 @dp.callback_query_handler(lambda c: c.data == 'button4')
 async def process_callback_button1(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, 'Мухомор пантреный',reply_markup = show_case_kb.inline_kb3)
-    await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
+    #await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
 
 @dp.callback_query_handler(lambda c: c.data == 'button5')
 async def process_callback_button1(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'Сколько надо? ',reply_markup = show_case_kb.inline_kb4)
-    await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
+    await bot.send_message(callback_query.from_user.id, 'Сколько надо? ',reply_markup = show_case_kb.keyb(1))
+    #await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
+@dp.callback_query_handler(lambda c: c.data == 'button12')
+async def process_callback_button1(callback_query: types.CallbackQuery,num = show_case_kb.num):
+    with suppress(MessageNotModified):
+        await bot.answer_callback_query(callback_query.id)
+        show_case_kb.num+=1
+        await callback_query.message.edit_text('Сколько надо?',reply_markup= show_case_kb.keyb(show_case_kb.num))
 
-
+@dp.callback_query_handler(lambda c: c.data == 'button7')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    show_case_kb.num = 1
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Выберите категорию продукта', reply_markup=show_case_kb.inline_kb)
+    #await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
 @dp.callback_query_handler(lambda c: c.data == 'button9')
 async def process_callback_button1(callback_query: types.CallbackQuery):
+    show_case_kb.num = 1
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, 'Вы вернулись в каталог',reply_markup = show_case_kb.inline_kb)
+    #await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
+@dp.callback_query_handler(lambda c: c.data == 'button13')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    show_case_kb.num = 1
     await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
-
-
 
 
 def register_handlers_client(dp : Dispatcher):
--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -1,34 +1,16 @@
-import sqlite3 as sq
+import sqlite3
+from data_base import base,cur
 from Handlers.create_bot import bot
-from Handlers import client
+async def sql_add_command(state):
+	async with state.proxy() as data:
+		cur.execute('INSERT INTO menu (category, name, description, price) VALUES (?, ?, ?, ?)', tuple(data.values()))
+		base.commit();
 
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
-
-
-
-class BotDB:
-
-    def __init__(self,db_file):
-        self.conn = sqlite3.connect(db_file)
-        self.cursor = self.conn.cursor()
-
-	def create_start():
-	base.execute('CREATE TABLE IF NOT EXISTS menu(id INTEGER PRIMARY KEY, category TEXT, name TEXT, description TEXT, price TEXT)')
-	#base.execute('CREATE TABLE IF NOT EXISTS korz(id INTEGER PRIMARY KEY, category TEXT, description TEXT, price TEXT)')
-	base.commit()
-
-	async def sql_add_command(state):
-		async with state.proxy() as data:
-			cur.execute('INSERT INTO menu VALUES (?, ?, ?, ?)', tuple(data.values()))
-			base.commit()
-
-	async def sql_imp_command(message):
-		for ret in cur.execute("SELECT * FROM menu").fetchall():
-			await bot.send_message(message.from_user.id, f"Название {ret[0]}\nКатегория {ret[1]}\nОписание {ret[2]}\nЦена {ret[3]}")
+async def sql_imp_command(message):
+	for ret in cur.execute("SELECT name, category, description, price FROM menu").fetchall():
+		await bot.send_message(message.from_user.id, f"Название {ret[0]}\nКатегория {ret[1]}\nОписание {ret[2]}\nЦена {ret[3]}")
 
 
-	async def sql_show_name(message):
-		for ret in cur.execute("SELECT name FROM menu").fetchall():
-			return ret[0]
+async def sql_show_name(message):
+	for ret in cur.execute("SELECT name FROM menu").fetchall():
+		return ret[0]
